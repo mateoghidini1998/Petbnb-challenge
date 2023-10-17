50b57b889eb437c6d361cd6d50bc2d98c17abddd
Mateo Ghidini <ghidinimateo1@gmail.com>
2023-10-17 19:30:56 -0300

    Add fontawesome icons

--- src/main.js
+++ src/main.js
@@ -2,4 +2,19 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+import { faDog, faCat, faHome, faKey, faSun, faSuitcase, faSearch } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faDog)
+library.add(faCat)
+library.add(faHome)
+library.add(faKey)
+library.add(faSun)
+library.add(faSuitcase)
+library.add(faSearch)
+
+
+createApp(App).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
+
