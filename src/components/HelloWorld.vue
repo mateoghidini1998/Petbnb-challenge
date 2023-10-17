ec631050eebdf53a3d343233e4d83bc384ca561e
Mateo Ghidini <ghidinimateo1@gmail.com>
2023-10-17 17:10:30 -0300

    Add Dog and Cat icons

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -7,9 +7,24 @@ defineProps({
 
 const count = ref(0)
 </script>
-
-<template>
   
+<template>
+  <div class="p-4 max-w-[880px]">
+    <div class="w-full flex flex-col">
+      <label for="">Pets</label>
+      <div class="flex w-1/2 justify-evenly">
+        <div class="rounded w-1/2 h-auto flex flex-col border-[1px] border-[#DBDDB] border-solid px-1">
+        <font-awesome-icon icon="user-secret" />
+        <font-awesome-icon icon="fa-solid fa-dog" />
+        Dog
+        </div>
+        <div class="rounded w-1/2 h-auto flex flex-col border-[1px] border-[#DBDDB] border-solid px-1">
+        <font-awesome-icon icon="fa-solid fa-cat" />
+        Cat
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped>
