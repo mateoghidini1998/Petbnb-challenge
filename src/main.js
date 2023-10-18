cc095c28c82b7078326dc9c3739913b8f090b2c9
Mateo Ghidini <ghidinimateo1@gmail.com>
2023-10-17 22:58:55 -0300

    Refactor imports, and add more icons

--- src/main.js
+++ src/main.js
@@ -5,7 +5,17 @@ import App from './App.vue'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-import { faDog, faCat, faHome, faKey, faSun, faSuitcase, faSearch } from '@fortawesome/free-solid-svg-icons'
+import { 
+    faDog, 
+    faCat, 
+    faHome, 
+    faKey, 
+    faSun, 
+    faSuitcase, 
+    faSearch, 
+    faPaw, 
+    faMapMarker 
+} from '@fortawesome/free-solid-svg-icons'
 
 library.add(faDog)
 library.add(faCat)
@@ -14,6 +24,8 @@ library.add(faKey)
 library.add(faSun)
 library.add(faSuitcase)
 library.add(faSearch)
+library.add(faPaw)
+library.add(faMapMarker)
 
 
 createApp(App).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
